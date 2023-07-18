ae1b3d0aef894a2f58cc8524d78258b6d4df4d57
namickey <teethdai@gmail.com>
2023-07-18 15:23:29 +0900

    mod

--- src/test/java/com/example/demo/web/ba01/ItemRegistViewHelperTest.java
+++ src/test/java/com/example/demo/web/ba01/ItemRegistViewHelperTest.java
@@ -24,6 +24,7 @@ public class ItemRegistViewHelperTest {
         " , "
     })
     public void test(String groupid, String label) {
+        
         assertEquals(label, itemRegistViewHelper.getGroupName(groupid));
     }
 }
